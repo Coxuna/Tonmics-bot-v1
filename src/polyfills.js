6b0f4f34942a0c5588ae042c6a874b79762601ba
Abuhaneeph <Lawanmuhammad0000@gmail.com>
2025-04-14 23:13:58 +0100

    correted buffer

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,13 @@
-// Create a new file: src/polyfills.js
+// src/polyfills.js
 import { Buffer } from 'buffer';
-window.Buffer = Buffer;
-global = window;
+
+if (typeof window !== 'undefined') {
+  if (!window.Buffer) {
+    window.Buffer = Buffer;
+  }
+
+  // Only define global if it doesn't already exist
+  if (typeof global === 'undefined') {
+    window.global = window;
+  }
+}
